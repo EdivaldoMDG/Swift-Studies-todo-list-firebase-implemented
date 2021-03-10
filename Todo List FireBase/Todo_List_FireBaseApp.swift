2006675808fa4212ce6a329066a0c92c411e434e
Edivaldo Goncalves <edivaldo.goncalves@chuva.io>
2021-03-10 15:43:17 -0100

    Initial Commit

---
+++ Todo List FireBase/Todo_List_FireBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_List_FireBaseApp.swift
+//  Todo List FireBase
+//
+//  Created by Edivaldo Goncalves on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Todo_List_FireBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
